6398e66b217749dcd1d7986f258508d7dc5473f2
unknown <Modifa>
2020-11-14 14:36:53 +0200

    Map

--- src/app/emergency-page/emergency-page.page.ts
+++ src/app/emergency-page/emergency-page.page.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var  google: any;
 @Component({
   selector: 'app-emergency-page',
   templateUrl: './emergency-page.page.html',
   styleUrls: ['./emergency-page.page.scss'],
 })
 export class EmergencyPagePage implements OnInit {
-
+map:any;
   constructor() { }
 
   ngOnInit() {
+    this.map = new google.maps.Map(document.getElementById('map' ) ,{
+    center: { lat: 45.518, lng: -122.672 },
+    zoom: 18,
+   //
+  });}
+    
   }
 
-}
