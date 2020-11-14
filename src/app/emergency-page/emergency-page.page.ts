5f58841bd6290bbc6837d00a27e2540635f34ed1
Kgotso Mampa <Kgotso.Mampa@vodacom2017.onmicrosoft.com>
2020-11-14 16:06:52 +0200

    location component

--- src/app/emergency-page/emergency-page.page.ts
+++ src/app/emergency-page/emergency-page.page.ts
@@ -1,7 +1,8 @@
 import { Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation';
 
-declare var  google: any;
+declare var google: any;
 @Component({
   selector: 'app-emergency-page',
   templateUrl: './emergency-page.page.html',
@@ -9,16 +10,53 @@ declare var  google: any;
 })
 export class EmergencyPagePage  {
   map:any;
+  longitude: any;
+  latitube: any;
+  isLoading: boolean = false;
+  @ViewChild('map') myMap;
   constructor(
     private location: Location
   ) { }
 
   ionViewWillEnter () {
-    this.map = new google.maps.Map(document.getElementById('map' ) ,{
-    center: { lat: 45.518, lng: -122.672 },
-    zoom: 18,
-   //
-  });}
+    this.setupCoordinates();
+}
+
+plotLocationOnMap(latitude, longitube){
+  this.map = new google.maps.Map(this.myMap.nativeElement ,{
+  center: { lat: latitude, lng: longitube },
+  zoom: 19,
+ //
+});
+}
+
+setupCoordinates(){
+  this.isLoading = true;
+  // Geolocation.watchPosition().subscribe(resp => {
+  //   if(resp){
+  //     this.isLoading = false;
+  //     this.latitube = (resp as any).coords.latitude
+  //     this.longitude = (resp as any).coords.longitude
+  //     this.plotLocationOnMap(this.latitube,this.longitude)
+  //     console.warn("resp watch Position",((resp as any).coords.latitude));
+  //   }
+  // },error => {
+  //   console.log('Error getting location', error);
+  //   this.isLoading = false;
+  // })
+  Geolocation.getCurrentPosition().then((resp) => {
+    if(resp){
+      this.isLoading = false;
+      this.latitube = resp.coords.latitude
+      this.longitude = resp.coords.longitude
+      this.plotLocationOnMap(this.latitube,this.longitude)
+      console.warn("resp.coords.longitude",resp.coords.longitude);
+    }
+   }).catch((error) => {
+     console.log('Error getting location', error);
+     this.isLoading = false;
+   });
+}
 
   back(){
     this.location.back();
